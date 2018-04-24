93c1a5f9c28b8b793610f101bfb3a73b01e56440
CheshtaK <32400008+CheshtaK@users.noreply.github.com>
2018-04-24 14:06:23 +0530

    login and register implemented

--- app/src/main/java/com/example/cheshta/motobuddy/LoginActivity.java
+++ app/src/main/java/com/example/cheshta/motobuddy/LoginActivity.java
@@ -1,13 +1,91 @@
 package com.example.cheshta.motobuddy;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.iid.FirebaseInstanceId;
 
 public class LoginActivity extends AppCompatActivity {
+
+    TextInputLayout tilYourEmail, tilLoginPassword;
+    Toolbar loginToolbar;
+    Button btnLogin;
+    ProgressDialog loginProgress;
+
+    FirebaseAuth mAuth;
+    DatabaseReference mUserDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        tilYourEmail = findViewById(R.id.tilYourEmail);
+        tilLoginPassword = findViewById(R.id.tilLoginPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+
+        loginToolbar = findViewById(R.id.loginToolbar);
+        setSupportActionBar(loginToolbar);
+        getSupportActionBar().setTitle("Login");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        loginProgress = new ProgressDialog(this);
+
+        mAuth = FirebaseAuth.getInstance();
+        mUserDatabase = FirebaseDatabase.getInstance().getReference().child("Users");
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = tilYourEmail.getEditText().getText().toString();
+                String password = tilLoginPassword.getEditText().getText().toString();
+
+                if(!email.isEmpty() && !password.isEmpty()){
+
+                    loginProgress.setTitle("Logging In");
+                    loginProgress.setMessage("Please wait while we check your credentials.");
+                    loginProgress.setCanceledOnTouchOutside(false);
+                    loginProgress.show();
+                    loginUser(email, password);
+                }
+            }
+        });
+    }
+
+    private void loginUser(String email, String password) {
+
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                if(task.isSuccessful()){
+                    loginProgress.dismiss();
+
+                    Intent mainIntent = new Intent(LoginActivity.this, MainActivity.class);
+                    mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(mainIntent);
+                    finish();
+                }
+                else {
+                    loginProgress.hide();
+                    Toast.makeText(LoginActivity.this, "Cannot sign in. Please check the form and try again", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/example/cheshta/motobuddy/MainActivity.java
+++ app/src/main/java/com/example/cheshta/motobuddy/MainActivity.java
@@ -1,13 +1,70 @@
 package com.example.cheshta.motobuddy;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    FirebaseAuth mAuth;
+    Toolbar mainToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mainToolbar = findViewById(R.id.mainToolbar);
+        setSupportActionBar(mainToolbar);
+        getSupportActionBar().setTitle("Motobuddy");
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser == null){
+            sendToStart();
+        }
+    }
+
+    private void sendToStart() {
+        Intent startIntent = new Intent(MainActivity.this, StartActivity.class);
+        startActivity(startIntent);
+        finish();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+
+        if(item.getItemId() == R.id.btnLogout){
+            FirebaseAuth.getInstance().signOut();
+            sendToStart();
+        }
+
+        if(item.getItemId() == R.id.btnHospital){
+
+        }
+
+        return true;
     }
 }
